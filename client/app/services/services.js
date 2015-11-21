ada0bb7a97f021468e53621de8b047a60aded39c
Adam Grimm <ATGrimm@gmail.com>
2015-11-21 15:50:15 -0800

    Reworked user feed page

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -6,34 +6,41 @@ angular.module('dartnews.services', [])
       url: '/api/user', 
       method: 'GET'
     });
-  }
+  };
 
   var getCategories = function (){
     return $http({
       url: '/api/categories', 
       method: 'GET'
     });
-  }
+  };
 
-  var updateUserCategories = function (category){
-    console.log(category);
+  var updateUserCategories = function (category, type){
     return $http({
       url: '/api/categories', 
-      method: 'PUT',
+      method: type,
       data: {
-        categories: {
-          category: category.toString()
-        }
+        category: category
       }
     });
-  }
+  };
 
   var getArticlesForUser = function (){
     return $http({
       url: '/api/articles', 
       method: 'GET'
     });
-  }
+  };
+
+  var likeArticle = function(articleID){
+    return $http({
+      url: '/api/user/like',
+      method: 'PUT',
+      data: {
+        articleID: articleID
+      }
+    });  
+  };
 
   return {
     getUserProfile: getUserProfile,
