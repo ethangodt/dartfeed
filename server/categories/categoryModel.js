abc43145ae5c259d64995ceb48ebd9c2f1155b40
Oliver Wang <oliverwangmusic@gmail.com>
2015-11-21 15:50:08 -0800

    refactored db and article controller

--- server/categories/categoryModel.js
+++ server/categories/categoryModel.js
@@ -1,20 +1,7 @@
 var mongoose = require('mongoose');
 
-var ArticleCategorySchema = new mongoose.Schema({
-  title : String,
-  linkURL: String,
-  summary: String,
-  source: String,
-  imgURL: String,
-  date: Date, 
-  visitsCount : Number,
-  metadata : String
-}, { _id : false});
-
 var CategorySchema = new mongoose.Schema({
-  name: String,
-  articles : [ArticleCategorySchema]
-
+  name: String
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
