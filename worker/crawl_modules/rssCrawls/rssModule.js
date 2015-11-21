259ef4b9db966de54b2b644175e368d48a478069
Ethan Godt <ethan@ethangodt.com>
2015-11-21 00:16:31 -0800

    general worker setup and monkeyTrainer progress

--- worker/crawl_modules/rssCrawls/rssModule.js
+++ worker/crawl_modules/rssCrawls/rssModule.js
@@ -1,15 +1,13 @@
-var FeedParser = require('feedparser');
 var request = require('request');
-var fs = require('fs');
-var feed = require("feed-read");
+var feed = require('feed-read');
 
-function feedParse(url,options){
+function feedParse(url, options){
   this.url = url;
   this.options = options;
 
   this.parse = function(callback){ //callback(err,articlesCollection)
     feed(this.url, callback);
-  }
+  };
 }
 
 module.exports = feedParse;
--- worker/monkeyTrainer.js
+++ worker/monkeyTrainer.js
@@ -1,3 +1,24 @@
-/**
- * Created by ethangodt on 11/20/15.
- */
+// This is the worker that pulls training samples from the trainingSamples collection of the db
+// (populated when a user likes an article), formats them, and sends them to monkey learn to teach about user preferences
+
+// this is used to connect this node instance's mongoose singleton to mongo server
+var db = require('./dbInit');
+var mongoose = require('mongoose');
+var trainingSampleCtrl = require('../server/trainingSample/trainingSampleController');
+var monkeyLearn = require('./analysis_module/UserTreeInterface');
+
+//var trainingSamples
+monkeyLearn.getUserCategoryIdsForAllTrees(function (payload) {
+  console.log(payload);
+});
+
+// pull all training samples and assign to variable
+trainingSampleCtrl.getAllTrainingSamples()
+  .then(function (trainingSamples) {
+    console.log(trainingSamples);
+  });
+
+// pull all the category information from the monkeyLearn api and assign to variable
+// pair ml and samples into array of objects like [{text: summary, categoryId: userId}]
+// call add samples function for each category from the database
+// as callback to addAllSamples call function to train monkey
