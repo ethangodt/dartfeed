de8e510637a57602d16ccba0dbf8c993fe8a18b7
AVickory <andrew.vickory@gmail.com>
2015-11-20 21:55:31 -0800

    got single tree samples, add user and get user category ids working.

--- worker/analysis_module/UserTreeInterface.js
+++ worker/analysis_module/UserTreeInterface.js
@@ -1,13 +1,13 @@
 var request = require('request');
 var _ = require('underscore');
 var token = require('../../server/config.js').monkeyLearnToken;
-var Promise = require('bluebird')
+var Promise = require('bluebird');
 
 var trees = {
   'Art & Culture': {id: 'cl_aPS8U36R'},
   business: {id: 'cl_3woeSYDA'},
   living: {id: 'cl_vgWv3zVm'},
-  'Science & Technology': {id: 'cl_7smzuZDg'}
+  'Science & Technology': {id: 'cl_7smzuZDg', rootId: 561573}
 };
 
 var numTrees = _.reduce(trees, function (acc, id) {
@@ -43,13 +43,12 @@ module.exports = {
       url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName].id + '/classify/?sandbox=1',
       headers: {
         'Authorization': 'token ' + token
-      },    
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
-      data : JSON.stringify({
+      },
+      json : JSON.stringify({
         text_list: articleArr
       })
-    }, function (err, categoryScores) {
+    }, function (err, res) {
+      var categoryScores = JSON.parse(res.body).result
       if(err) {
         console.log('error in userTree.classify:', err)
       } else {
@@ -62,12 +61,10 @@ module.exports = {
       url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName].id + '/samples/',
       headers: {
         'Authorization': 'token ' + token
-      },    
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
-      data : JSON.stringify({
-        text_list: {samples: dbTrainingArr}
-      })
+      },
+      json: {
+        samples: dbTrainingArr
+      }
     }, function (err) {
       if(err) {
         console.log('error in userTree.addSamples:', err)
@@ -76,14 +73,12 @@ module.exports = {
       }
     });
   },
-  startTraining: function (treeName, successCallback) {
+  startTraining: function (treeName, callback) {
     request.post({
       url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName].id + '/train/',
       headers: {
         'Authorization': 'token ' + token
-      },    
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
+      },
     }, function (err) {
       if(err) {
         console.log('error in userTree.startTraining:', err)
@@ -97,18 +92,16 @@ module.exports = {
       url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName].id + '/',
       headers: {
         'Authorization': 'token ' + token
-      },    
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
+      },
     }, function (err, res) {
       if(err) {
         console.log('error in userTree.getUserCategoryIdsForTree:', err)
       } else {
-        var userNameToId = _.reduce(res.result.sandbox_categories, function (acc, catObj) {
+        var userNameToId = _.reduce(JSON.parse(res.body).result.sandbox_categories, function (acc, catObj) {
           acc[catObj.name] = catObj.id;
           return acc;
         }, {}); //includes the root id
-        trees[treeName].rootId = userNameToId[root];
+        trees[treeName].rootId = userNameToId['Root'];
         callback(userNameToId);
       }
     });
@@ -116,26 +109,19 @@ module.exports = {
   addUserToTree: function (treeName, username, callback) {
     //must have called getUserCategoryIdsForTree to have the root id setfor this treeName
     request.post({
-      url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName] + '/categories/',
+      url: 'https://api.monkeylearn.com/v2/classifiers/' + trees[treeName].id + '/categories/',
       headers: {
         'Authorization': 'token ' + token
-      },    
-      dataType: "json",
-      contentType: "application/json; charset=utf-8",
-      data : JSON.stringify({
-        text_list: {
-          name: username, 
-          parent_id: trees[treeName].rootId
-        }
-      })
+      },
+      json: {
+        "name": username, 
+        "parent_id": trees[treeName].rootId
+      }
     }, function (err, res) {
-      if(err) {
-        console.log('error in userTree.getUserCategoryIdsForTree:', err)
+      console.log(res.statusCode)
+      if(err || res.statusCode >= 400) {
+        console.log('error in userTree.addUserToTree:', err)
       } else {
-        var userNameToId = _.reduce(res.result.sandbox_categories, function (acc, catObj) {
-          acc[catObj.name] = catObj.id;
-          return acc;
-        }, {}); //includes the root id
         callback();
       }
     });
@@ -148,34 +134,22 @@ module.exports['addAllSamples'] = execOnAllTrees(module.exports.addSamples);
 
 var sample = {
   text: "A study in the Netherlands backs up a long-held claim of quantum theory, one that Einstein refused to accept, that objects separated by great distance could affect each other\u2019s behavior.","abstract":"Scientists from Delft University of Technology in the Netherlands publish study in journal Nature finding objects separated by great distances can instantaneously affect each other\u2019s behavior, proving one of most fundamental claims of quantum theory",
-  user = 'test'
-  tree = 'Science & Technology'
+  user: "test",
+  tree: 'Science & Technology'
 }
 
-var app = express();
-var expressRouter = express.Router(); 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(logger);
-
-app.listen(80); 
-
 module.exports.getUserCategoryIdsForTree(sample.tree, function (res) {
-  console.log('\ngetUserCategoryIdsForTree : ', res);
-  module.exports.addUserToTree(sample.tree, sample.user, function () {
-    console.log('\nLOOK FOR NEW USER IN ' + sample.tree + ' NAMED ' + sample.user + '!!!\n');
-    module.exports.getUserCategoryIdsForTree(sample.tree, function (res) {
-      console.log('\ngetUserCategoryIdsForTree with user added: ', res);
-      var 
-      module.exports.addSamples(sample.tree, [{text: sample.text, category_id: res[sample.user]}], function () {
-        console.log('\nadded Samples!')
-        module.exports.startTraining(sample.tree, function () {
-          console.log('\n Training Started!')
-        })
-      })
+  
+  module.exports.addSamples(sample.tree, [{text: sample.text, category_id: res['Root']}, {text: sample.text, category_id: res[sample.user]}], function () {
+
+    console.log('\nadded Samples!')
+
+    module.exports.startTraining(sample.tree, function () {
+
+      console.log('\n Training Started!')
+
     })
+
   })
 })
 
