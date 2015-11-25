7adf61a9d07a12c724da2e2c0750bfa58b5c0fa9
Oliver Wang <oliverwangmusic@gmail.com>
2015-11-24 23:14:22 -0800

    implemented userLikes with persistant like status on frontend

--- server/articles/articleController.js
+++ server/articles/articleController.js
@@ -9,22 +9,27 @@ Promise.promisifyAll(require('mongoose'));
 var getArticles = function (req, res, next) {
   User.findOne({_id: req.user.id})
     .exec(function (err, user){
-      var categories = user.categories;
+      var categories = user.categories.filter(function (item){
+        return !!item;
+      });
       var resBody = {};
       resBody.articles = [];
       Article.find()
         .in('categories',categories)
         .then(function (articles) {
           articles.forEach(function (art) {
-            art.userLikes = !!art.userLikes[req.user.id];
+            art.userLikes = !!art.userLikes && !!art.userLikes[req.user.id];
           });
+          console.log('articles', articles)
           resBody.articles = articles;
           resBody.userCats = categories;
+          console.log('resBody', resBody)
           Category.find({})
             .then(function (cats){
               resBody.allCats = cats.map(function(cat){
                 return cat.name;
               });
+              console.log('resBody', resBody)
               res.json(resBody);
             });
         });
