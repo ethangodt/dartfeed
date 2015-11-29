8e00af69dd86e1948d53d304dd6bc92732eab677
Oliver Wang <oliverwangmusic@gmail.com>
2015-11-29 14:58:26 -0800

    edited gruntfile for renaming and hardcoded categories

--- server/categories/categoryModel.js
+++ server/categories/categoryModel.js
@@ -4,4 +4,24 @@ var CategorySchema = new mongoose.Schema({
   name: String
 });
 
-module.exports = mongoose.model('Category', CategorySchema);
+var cats = mongoose.model('Category', CategorySchema);
+var docArray = ['Arts & Culture',
+'Business',
+'Living',
+'Science & Technology',
+'Sports',
+'World'];
+
+cats.find()
+  .then(function (allCats){
+    if (allCats.length < 6){
+      for (var i = docArray.length - 1; i >= 0; i--) {
+        var name = docArray[i];
+        var newCat = new cats({name:name});
+        newCat.save();
+      }
+    }
+  });
+
+
+module.exports = cats;
