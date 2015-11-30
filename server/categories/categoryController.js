087509d30c4cb7cb2c08e7dec688a4bc4c321c32
Ethan Godt <ethan@ethangodt.com>
2015-11-29 20:18:18 -0800

    Fixed controller nesting and minor angular bugs

--- server/categories/categoryController.js
+++ server/categories/categoryController.js
@@ -3,34 +3,32 @@ var User = require('../users/userModel.js');
 var Article = require('../articles/articleController');
 
 module.exports = {
-  getCategories: function (req, res, next){
-    Category.find(function (err, cats){
+  getCategories: function (req, res, next) {
+    Category.find(function (err, cats) {
       res.send(cats);
     });
   },
 
-  updateUserCategories: function(req, res, next){
-    User.findOne({_id: req.user.id}, function (err, user){
-      if(err) {
+  updateUserCategories: function (req, res, next) {
+    User.findOne({_id: req.user.id}, function (err, user) {
+      if (err) {
         res.send(err);
       }
-      if (req.body.type === 'PUT'){
-        if (user.categories.indexOf(req.body.category) === -1){
+      if (req.body.type === 'PUT') {
+        if (user.categories.indexOf(req.body.category) === -1) {
           user.categories.push(req.body.category);
         }
         user.save()
-          .then(function(){
+          .then(function () {
             Article.getArticles(req, res, next);
           });
-      } else if (req.body.type === "POST"){
-        user.categories.splice(user.categories.indexOf(req.body.category),1);
+      } else if (req.body.type === "POST") {
+        user.categories.splice(user.categories.indexOf(req.body.category), 1);
         user.save()
           .then(function () {
-            Article.getArticles(req,res,next);
+            Article.getArticles(req, res, next);
           });
       }
     });
   }
 };
-
-
